907985eeb3ae89c5f48e091418bdb2483090e7b3
Alisa <momoirokutsyne@gmail.com>
2021-11-24 17:27:12 +0300

    Сигнатуры методов класса MessagesImpl определены в интерфейсе Messages

--- src/interfaces/Messages.java
+++ src/interfaces/Messages.java
@@ -1,4 +1,8 @@
 package interfaces;
 
 public interface Messages {
+    void descriptionMessage(String[] words, int length, String name);
+    void actionMessage(String action, String name);
+    void endOfSentence(int number);
+    void tempMessage(String name, String property);
 }
